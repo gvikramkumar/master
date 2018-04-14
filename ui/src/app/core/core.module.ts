1c071da0c3a7f19f1de7731772df17dfd10d2710
dan kahle <dan.kahle@gmail.com>
2018-04-14 13:39:13 -0700

    added config files and fixed graphql exnext.asyncIterable bug

--- ui/src/app/core/core.module.ts
+++ ui/src/app/core/core.module.ts
@@ -1,20 +1,45 @@
-import {Inject, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {Store} from '../store/store';
-import {ProgressService} from './services/progress.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {SpinnerInterceptor} from './interceptors/spinner.interceptor';
-import {TimingInterceptor} from './interceptors/timing.interceptor';
-import {ErrorInterceptor} from './interceptors/error.interceptor';
-import {Init1, Init2, Init3, Init4, Init5} from './services/init-service';
-import {ModifyRequestInterceptor} from './interceptors/modify-request.interceptor';
-import {InitializationGuard} from '../routing/guards/initialization.guard';
-import {AuthGuard} from '../routing/guards/auth.guard';
-import {UserService} from './services/user-service';
-import {RouterModule} from '@angular/router';
-import {ErrorStateMatcher} from '@angular/material';
-import {StoreModule} from '../store/store.module';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {Store} from "../store/store";
+import {ProgressService} from "./services/progress.service";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {SpinnerInterceptor} from "./interceptors/spinner.interceptor";
+import {TimingInterceptor} from "./interceptors/timing.interceptor";
+import {ErrorInterceptor} from "./interceptors/error.interceptor";
+import {Init1, Init2, Init3, Init4, Init5} from "./services/init-service";
+import {ModifyRequestInterceptor} from "./interceptors/modify-request.interceptor";
+import {InitializationGuard} from "../routing/guards/initialization.guard";
+import {AuthGuard} from "../routing/guards/auth.guard";
+import {RouterModule} from "@angular/router";
+import {StoreModule} from "../store/store.module";
 import {BreakpointService} from "./services/breakpoint.service";
+import {Apollo} from "apollo-angular";
+import {HttpLink} from "apollo-angular-link-http";
+import {environment} from "../../environments/environment";
+import {InMemoryCache} from "apollo-cache-inmemory";
+import {DefaultOptions} from "apollo-client/ApolloClient";
+
+// working with the apollo cache is somewhat complicated and error prone. You require cache resolvers and
+// query updates on add/delete mutations to make it work. We don't have much to get so until the use
+// case presents itself, we'll simplify things with no-cache, i.e. all queries will go to the server for now
+// (use apollo.query instead of apollo.watchQuery), and nothing will be cached (no reason then).
+// If on the other hand, you wish to implement this, then this bookeeping is required and if watchQuery is used
+// it will notify the subscribers on any mutation.
+// https://www.apollographql.com/docs/angular/features/cache-updates.html#cacheRedirect
+// https://www.apollographql.com/docs/angular/features/cache-updates.html
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+  mutate: {
+    errorPolicy: 'all',
+  },
+};
 
 @NgModule({
   imports: [
@@ -34,6 +59,14 @@ import {BreakpointService} from "./services/breakpoint.service";
   ]
 })
 export class CoreModule {
-  constructor() {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({uri: environment.apiUrl + '/api/graphql'}),
+      cache: new InMemoryCache(),
+      defaultOptions
+    });
   }
 }
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'http://localhost:3000',
   logState: false,
-  disableAnimations: true
+  disableAnimations: false
 };
