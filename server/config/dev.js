858d0ff3e530a9d0006a4929b1aacc12240c3280
dan kahle <dan.kahle@gmail.com>
2018-04-18 17:11:07 -0700

    page scroll fixed

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: 3002,
   mongoUri: 'mongodb://localhost/fin-dfa',
   postgresUri: null,
   corsOrigin: ['http://localhost:4200'],
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -8,7 +8,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3002',
   logState: false,
   disableAnimations: false
 };
