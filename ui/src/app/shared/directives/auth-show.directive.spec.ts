9cbe3e5e712a9263b7f8bad1f34aff6c12db847a
dan kahle <dan.kahle@gmail.com>
2018-05-07 16:48:59 -0700

    auth directives working

---
+++ ui/src/app/shared/directives/auth-show.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AuthDirective } from './auth-show.directive';
+
+describe('AuthDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AuthDirective();
+    expect(directive).toBeTruthy();
+  });
+});
