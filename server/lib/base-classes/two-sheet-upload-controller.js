406ab51bd72b8ff9daf985fdb67e8cb64433cef1
dan kahle <dan.kahle@gmail.com>
2018-06-06 10:27:22 -0700

    US187994: oneSheetUploadController working for dollar/mapping

---
+++ server/lib/base-classes/two-sheet-upload-controller.js
@@ -0,0 +1,280 @@
+const xlsx = require('node-xlsx'),
+  NamedApiError = require('../common/named-api-error'),
+  ApiError = require('../common/api-error'),
+  _ = require('lodash'),
+  mail = require('../common/mail'),
+  OpenPeriodRepo = require('../../api/common/open-period/repo'),
+  UploadController = require('./upload-controller');
+
+const UploadValidationError = 'UploadValidationError';
+const openPeriodRepo = new OpenPeriodRepo();
+
+module.exports = class TwoSheetUploadController extends UploadController {
+
+  constructor(repo) {
+    super(repo);
+  }
+
+  upload2Sheets(req, res, next) {
+    this.startUpload = Date.now();
+    this.req = req;
+    this.userId = req.user.id;
+    const sheets = xlsx.parse(req.file.buffer);
+    this.rows1 = sheets[0].data.slice(5).filter(row => row.length > 1); // might be comment row in there
+    this.rows2 = sheets[1].data.slice(5).filter(row => row.length > 1); // might be comment row in there
+    this.totalErrors1 = {};
+    this.totalErrors2 = {};
+    this.hasTotalErrors1 = false;
+    this.hasTotalErrors2 = false;
+    if (this.rows1.length < 1) { // assumes the import data is in sheet 1
+      next(new ApiError('No records to upload. Please use the appropriate upload template, entering records after line 5.', null, 400));
+      return;
+    } else if (!(this.rowColumnCount === Object.keys(this.rows[0]).length)) {
+      next(new ApiError(`Incorrect column count for ${this.uploadName}. Are you sending up the correct template?`, null, 400));
+      return;
+    } else {
+      res.end();
+    }
+
+    this.getValidationAndImportData()
+      .then(() => {
+        return this.validateRows2Sheets()
+          .then(() => {
+            if (this.hasTotalErrors) {
+              return Promise.reject(new NamedApiError(UploadValidationError));
+            }
+          })
+      })
+      .then(() => this.importRows())
+      .then(() => this.sendSuccessEmail())
+      .catch(err => {
+        if (err && err.name === UploadValidationError) {
+          this.sendValidationEmail();
+        } else {
+          this.sendErrorEmail(err);
+        }
+      });
+
+  }
+
+  validateRows2Sheets() {
+    let chain = Promise.resolve();
+
+    this.rows1.forEach((row, idx) => {
+      chain = chain.then(() => this.validateRowSheet1(row, idx + 6));
+    });
+    this.rows2.forEach((row, idx) => {
+      chain = chain.then(() => this.validateRowSheet2(row, idx + 6));
+    });
+    return chain;
+  }
+
+  validateRowSheet1(row, rowNum) {
+    this.errors = [];
+    this.rowNum = rowNum;
+
+    return this.validateSheet1(row)
+      .catch(err => {
+        if (err.name === UploadValidationError) {
+          this.totalErrors['Row ' + this.rowNum] = err.data;
+          this.hasTotalErrors = true;
+          if (Object.keys(this.totalErrors).length > 99) {
+            return Promise.reject(err); // send validation email
+          }
+
+        } else {
+          return Promise.reject(err); // send error email
+        }
+      })
+  }
+
+  validateRowSheet2(row, rowNum) {
+    this.errors = [];
+    this.rowNum = rowNum;
+
+    return this.validateSheet2(row)
+      .catch(err => {
+        if (err.name === UploadValidationError) {
+          this.totalErrors['Row ' + this.rowNum] = err.data;
+          this.hasTotalErrors = true;
+          if (Object.keys(this.totalErrors).length > 99) {
+            return Promise.reject(err); // send validation email
+          }
+
+        } else {
+          return Promise.reject(err); // send error email
+        }
+      })
+  }
+
+  validateRows() {
+    let chain = Promise.resolve();
+
+    this.rows.forEach((row, idx) => {
+      chain = chain.then(() => this.validateRow(row, idx + 6));
+    });
+    return chain;
+  }
+
+  validateRow(row, rowNum) {
+    this.errors = [];
+    this.rowNum = rowNum;
+
+    return this.validate(row)
+      .catch(err => {
+        if (err.name === UploadValidationError) {
+          this.totalErrors['Row ' + this.rowNum] = err.data;
+          this.hasTotalErrors = true;
+          if (Object.keys(this.totalErrors).length > 99) {
+            return Promise.reject(err); // send validation email
+          }
+
+        } else {
+          return Promise.reject(err); // send error email
+        }
+      })
+  }
+
+  lookForErrors() {
+    if (this.errors.length) {
+      return Promise.reject(new NamedApiError(UploadValidationError, null, _.sortBy(this.errors, 'property')));
+    }
+    return Promise.resolve();
+  }
+
+  importRows() {
+    const imports = this.rows.map(row => this.getImportDoc(row));
+    return this.repo.addManyTransaction(imports);
+  }
+
+  getValidationAndImportData() {
+    return Promise.all([
+      openPeriodRepo.getOne()
+        .then(doc => this.fiscalMonth = doc.fiscalMonth)
+    ]);
+  }
+
+  sendEmail(title, body) {
+    return mail.send(
+      this.req.user.email,
+      this.req.user.email,
+      title,
+      null,
+      body
+    );
+  }
+
+  sendErrorEmail(err) {
+    this.sendEmail(`${this.uploadName} - Upload Error`, this.buildErrorEmailBody(err));
+  }
+
+  sendValidationEmail() {
+    this.sendEmail(`${this.uploadName} - Validation Errors`, this.buildValidationEmailBody());
+  }
+
+  sendSuccessEmail() {
+    this.sendEmail(`${this.uploadName} - Success`, this.buildSuccessEmailBody());
+  }
+
+  buildSuccessEmailBody() {
+    const duration = Math.round((Date.now() - this.startUpload) / 1000);
+    return `<div>${this.rows.length} records successfully uploaded in ${duration} seconds.</div>`
+  }
+
+  buildErrorEmailBody(err) {
+    const obj = Object.assign({}, err);
+    if (err.message) {
+      obj.message = err.message;
+    }
+    if (err.stack) {
+      obj.stack = err.stack;
+    }
+    return `
+    <div>An unexpected error occured during upload:</div>
+    <pre>
+      ${JSON.stringify(obj, null, 2)}
+    </pre>`
+  }
+
+  buildValidationEmailBody() {
+    let first = true;
+    let body = '';
+    _.forEach(this.totalErrors, (val, key) => {
+      if (first) {
+        first = false;
+        body += '<br>';
+      } else {
+        body += '<br><br>';
+      }
+      body += '<div style="font-size:18px;">' + key + '</div><hr><table>';
+      val.forEach(err => {
+        if (err.property) {
+          let append = `<tr><td style="width: 300px; margin-right: 30px">${err.property}:</td>
+                <td style="width: 300px; margin-right: 30px">${err.error}</td>`;
+          if (err.value) {
+            append += `<td style="width: 300px;">${err.value}</td>`
+          }
+          append += '</tr>';
+          body += append;
+        } else {
+          body += `<tr><td colspan="2" style="width:630px">* ${err.error}</td></tr>`
+        }
+      })
+      body += '</table>'
+    });
+    return body;
+  }
+
+  addError(property, message, value) {
+    const obj = {property, error: message};
+    if (value) {
+      obj.value = value;
+    }
+    this.errors.push(obj);
+  }
+
+  addErrorRequired(property) {
+    this.addError(property, 'Required')
+  }
+
+  addErrorInvalid(property, value, acceptableValues) {
+    let msg = 'Invalid value';
+    if (acceptableValues) {
+      msg += ` (${acceptableValues})`;
+    }
+    this.addError(property, msg, value)
+  }
+
+  addErrorRequiredForItem(property, item) {
+    this.addError(property, `${property} is required for this ${item}`)
+  }
+
+  addErrorRequiredForSubmeasure(property) {
+    this.addErrorRequiredForItem(property, 'submeasure');
+  }
+
+  addErrorNotAllowedForItem(property, item) {
+    this.addError(property, `${property} is not allowed for this ${item}`)
+  }
+
+  addErrorNotAllowedForSubmeasure(property) {
+    this.addErrorNotAllowedForItem(property, 'submeasure');
+  }
+
+  notExists(values, value) {
+    return _.sortedIndexOf(values, value.toUpperCase()) === -1;
+  }
+
+  validateNumber(prop, val, required) {
+    if (required && (val === undefined || val === '')) {
+      this.addErrorRequired(prop);
+      return false;
+    } else if (Number.isNaN(Number(val))) {
+      this.addError(prop, 'Not a number', val);
+      return false;
+    }
+    return true;
+  }
+
+}
+
--- server/lib/base-classes/upload-controller.js
+++ server/lib/base-classes/upload-controller.js
@@ -1,6 +1,4 @@
-const xlsx = require('node-xlsx'),
-  NamedApiError = require('../common/named-api-error'),
-  ApiError = require('../common/api-error'),
+const NamedApiError = require('../common/named-api-error'),
   _ = require('lodash'),
   mail = require('../common/mail'),
   OpenPeriodRepo = require('../../api/common/open-period/repo');
@@ -14,175 +12,11 @@ module.exports = class UploadController {
     this.repo = repo;
   }
 
-  upload(req, res, next) {
-    this.startUpload = Date.now();
-    this.req = req;
-    this.userId = req.user.id;
-    const sheets = xlsx.parse(req.file.buffer);
-    this.rows = sheets[0].data.slice(5).filter(row => row.length > 1); // might be comment row in there
-    this.totalErrors = {};
-    this.hasTotalErrors = false;
-    if (this.rows.length < 1) {
-      next(new ApiError('No records to upload. Please use the appropriate upload template, entering records after line 5.', null, 400));
-      return;
-    } else if (!(this.rowColumnCount === Object.keys(this.rows[0]).length)) {
-      next(new ApiError(`Incorrect column count for ${this.uploadName}. Are you sending up the correct template?`, null, 400));
-      return;
-    } else {
-      res.end();
-    }
-
-    this.getValidationAndImportData()
-      .then(() => {
-        return this.validateRows()
-          .then(() => {
-            if (this.hasTotalErrors) {
-              return Promise.reject(new NamedApiError(UploadValidationError));
-            }
-          })
-      })
-      .then(() => this.importRows())
-      .then(() => this.sendSuccessEmail())
-      .catch(err => {
-        if (err && err.name === UploadValidationError) {
-          this.sendValidationEmail();
-        } else {
-          this.sendErrorEmail(err);
-        }
-      });
-
-  }
-
-  upload2Sheets(req, res, next) {
-    this.startUpload = Date.now();
-    this.req = req;
-    this.userId = req.user.id;
-    const sheets = xlsx.parse(req.file.buffer);
-    this.rows1 = sheets[0].data.slice(5).filter(row => row.length > 1); // might be comment row in there
-    this.rows2 = sheets[1].data.slice(5).filter(row => row.length > 1); // might be comment row in there
-    this.totalErrors1 = {};
-    this.totalErrors2 = {};
-    this.hasTotalErrors1 = false;
-    this.hasTotalErrors2 = false;
-    if (this.rows1.length < 1) { // assumes the import data is in sheet 1
-      next(new ApiError('No records to upload. Please use the appropriate upload template, entering records after line 5.', null, 400));
-      return;
-    } else if (!(this.rowColumnCount === Object.keys(this.rows[0]).length)) {
-      next(new ApiError(`Incorrect column count for ${this.uploadName}. Are you sending up the correct template?`, null, 400));
-      return;
-    } else {
-      res.end();
-    }
-
-    this.getValidationAndImportData()
-      .then(() => {
-        return this.validateRows2Sheets()
-          .then(() => {
-            if (this.hasTotalErrors) {
-              return Promise.reject(new NamedApiError(UploadValidationError));
-            }
-          })
-      })
-      .then(() => this.importRows())
-      .then(() => this.sendSuccessEmail())
-      .catch(err => {
-        if (err && err.name === UploadValidationError) {
-          this.sendValidationEmail();
-        } else {
-          this.sendErrorEmail(err);
-        }
-      });
-
-  }
-
-  validateRows2Sheets() {
-    let chain = Promise.resolve();
-
-    this.rows1.forEach((row, idx) => {
-      chain = chain.then(() => this.validateRowSheet1(row, idx + 6));
-    });
-    this.rows2.forEach((row, idx) => {
-      chain = chain.then(() => this.validateRowSheet2(row, idx + 6));
-    });
-    return chain;
-  }
-
-  validateRowSheet1(row, rowNum) {
-    this.errors = [];
-    this.rowNum = rowNum;
-
-    return this.validateSheet1(row)
-      .catch(err => {
-        if (err.name === UploadValidationError) {
-          this.totalErrors['Row ' + this.rowNum] = err.data;
-          this.hasTotalErrors = true;
-          if (Object.keys(this.totalErrors).length > 99) {
-            return Promise.reject(err); // send validation email
-          }
-
-        } else {
-          return Promise.reject(err); // send error email
-        }
-      })
-  }
-
-  validateRowSheet2(row, rowNum) {
-    this.errors = [];
-    this.rowNum = rowNum;
-
-    return this.validateSheet2(row)
-      .catch(err => {
-        if (err.name === UploadValidationError) {
-          this.totalErrors['Row ' + this.rowNum] = err.data;
-          this.hasTotalErrors = true;
-          if (Object.keys(this.totalErrors).length > 99) {
-            return Promise.reject(err); // send validation email
-          }
-
-        } else {
-          return Promise.reject(err); // send error email
-        }
-      })
-  }
-
-  validateRows() {
-    let chain = Promise.resolve();
-
-    this.rows.forEach((row, idx) => {
-      chain = chain.then(() => this.validateRow(row, idx + 6));
-    });
-    return chain;
-  }
-
-  validateRow(row, rowNum) {
-    this.errors = [];
-    this.rowNum = rowNum;
-
-    return this.validate(row)
-      .catch(err => {
-        if (err.name === UploadValidationError) {
-          this.totalErrors['Row ' + this.rowNum] = err.data;
-          this.hasTotalErrors = true;
-          if (Object.keys(this.totalErrors).length > 99) {
-            return Promise.reject(err); // send validation email
-          }
-
-        } else {
-          return Promise.reject(err); // send error email
-        }
-      })
-  }
-
   lookForErrors() {
     if (this.errors.length) {
       return Promise.reject(new NamedApiError(UploadValidationError, null, _.sortBy(this.errors, 'property')));
     }
     return Promise.resolve();
-  }
-
-  importRows() {
-    const imports = this.rows.map(row => this.getImportDoc(row));
-    return this.repo.addManyTransaction(imports);
   }
 
   getValidationAndImportData() {
@@ -232,35 +66,6 @@ module.exports = class UploadController {
     <pre>
       ${JSON.stringify(obj, null, 2)}
     </pre>`
-  }
-
-  buildValidationEmailBody() {
-    let first = true;
-    let body = '';
-    _.forEach(this.totalErrors, (val, key) => {
-      if (first) {
-        first = false;
-        body += '<br>';
-      } else {
-        body += '<br><br>';
-      }
-      body += '<div style="font-size:18px;">' + key + '</div><hr><table>';
-      val.forEach(err => {
-        if (err.property) {
-          let append = `<tr><td style="width: 300px; margin-right: 30px">${err.property}:</td>
-                <td style="width: 300px; margin-right: 30px">${err.error}</td>`;
-          if (err.value) {
-            append += `<td style="width: 300px;">${err.value}</td>`
-          }
-          append += '</tr>';
-          body += append;
-        } else {
-          body += `<tr><td colspan="2" style="width:630px">* ${err.error}</td></tr>`
-        }
-      })
-      body += '</table>'
-    });
-    return body;
   }
 
   addError(property, message, value) {
