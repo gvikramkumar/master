5d02b1f9c234dd9bceaaf155e2179a5624e723db
dan kahle <dan.kahle@gmail.com>
2018-06-25 14:29:06 -0700

    chore: fix database scripts

--- database/seed.sh
+++ database/seed.sh
@@ -2,13 +2,13 @@
 
 # must be called with: host port database
 
-mongo --eval "var host='$1', port='$2', _db='$3'" create-collections.js
+mongo --nodb  --eval "var host='$1', port='$2', _db='$3'" create-collections.js
 
 mongoimport --host $1 --port $2 --db $3  --collection dfa_allocation_rule --type=csv --headerline --file data/allocation_rule.csv
 mongoimport --host $1 --port $2 --db $3 --collection dfa_module --type=csv --headerline --file data/module.csv
 
 echo database load complete
 
-mongo --eval "var host='$1', port='$2', _db='$3'" post-data-load.js
+mongo --nodb  --eval "var host='$1', port='$2', _db='$3'" post-data-load.js
 
 node load-files.js $1 $2 $3
