33ff0877f7416a5e307642249b5c0c7cb195fa04
dan kahle <dan.kahle@gmail.com>
2018-06-28 16:16:31 -0700

    chore: add lookup services: revert app component test code

--- ui/src/app/app/app.component.ts
+++ ui/src/app/app/app.component.ts
@@ -1,15 +1,10 @@
-import {AfterViewInit, Component, HostBinding, OnInit, ViewChild} from "@angular/core";
-import {environment} from "../../environments/environment";
+import {AfterViewInit, Component, HostBinding, OnInit, ViewChild} from '@angular/core';
+import {environment} from '../../environments/environment';
 import {NavigationEnd, NavigationStart, Router} from '@angular/router';
 import {CuiHeaderOptions, CuiToastComponent} from '@cisco-ngx/cui-components';
 import {Title} from '@angular/platform-browser';
 import {AppStore} from './app-store';
 import {ToastService} from '../core/services/toast.service';
-import {LookupService} from '../modules/_common/services/lookup.service';
-import {HttpParams} from '@angular/common/http';
-import {ModuleLookupService} from '../modules/_common/services/module-lookup.service';
-
-const key = 'stuff';
 
 @Component({
   selector: 'fin-root',
@@ -22,57 +17,17 @@ export class AppComponent implements OnInit, AfterViewInit {
   title = 'fin-dfa';
   @ViewChild('permToast') permToast: CuiToastComponent;
   @ViewChild('autoHideToast') autoHideToast: CuiToastComponent;
-  val: any;
-  noerror = false;
 
-  constructor(private ls: LookupService, private titleService: Title, public store: AppStore, private toastService: ToastService) {
+  constructor(private titleService: Title, public store: AppStore, private toastService: ToastService) {
   }
-
-  get() {
-    if (this.noerror) {
-      this.ls.getNoError(key).subscribe(val => this.val = val);
-    } else {
-      this.ls.getWithError(key).subscribe(val => this.val = val);
-    }
-  }
-  add() {
-    this.ls.add(key, this.val).subscribe(_val => this.val = _val);
-  }
-
-  upsert() {
-    this.ls.upsert(key, this.val).subscribe(_val => this.val = _val);
-  }
-
-  remove() {
-    this.ls.remove(key).subscribe(val => this.val = val);
-  }
-
-
-
-
 
   public ngOnInit() {
-    this.titleService.setTitle('FIN-DFA')
+    this.titleService.setTitle('FIN-DFA');
   }
 
   public ngAfterViewInit() {
     this.toastService.permToast = this.permToast;
     this.toastService.autoHideToast = this.autoHideToast;
   }
-
-  headerOptions = new CuiHeaderOptions({
-    "showBrandingLogo": true,
-    "brandingLink": "https://cisco.com",
-    "brandingTitle": "",
-    "showMobileNav": true,
-    "title": "Digitized Financial Allocations",
-    "breadcrumbs": [
-      {
-        "label": "Home",
-        "url": "dfa"
-      }
-    ],
-    "username": "Maryellen Oltman",
-  });
 
 }
