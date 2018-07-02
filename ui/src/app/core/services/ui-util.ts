b522a88d9cfe039380d61e3473c491fad283b79d
dan kahle <dan.kahle@gmail.com>
2018-07-02 13:59:11 -0700

    submeasure submit: fix yearmo calculation for look like what ram wanted

--- ui/src/app/core/services/ui-util.ts
+++ ui/src/app/core/services/ui-util.ts
@@ -13,18 +13,53 @@ export const uiUtil = {
 
 function getFiscalMonthListFromDate(date, numMonths) {
   const yearmos = [];
-  const months = _.range(date.getMonth() + 5, date.getMonth() + 5 - numMonths);
-  let month;
+  const curMonths = _.range(date.getMonth(), date.getMonth() - numMonths);
+  const fisMonths = _.range(date.getMonth() + 5, date.getMonth() + 5 - numMonths);
 
-  months.forEach(mon => {
-    date.setMonth(mon);
-    month = date.getMonth() + 1;
-    yearmos.push('' + date.getFullYear() + (month < 10 ? '0' + month : month));
-  })
+  for (let i = 0; i < numMonths; i++) {
+    const curDate = new Date(date.getTime());
+    const fisDate = new Date(date.getTime());
+    curDate.setMonth(curMonths[i]);
+    const curYear = curDate.getFullYear();
+    const curMonthNum = curDate.getMonth() + 1;
+    const curMonthName = getMonthNameFromNum(curMonthNum);
+    fisDate.setMonth(fisMonths[i]);
+    const fisYear = fisDate.getFullYear();
+    const fisMonth = fisDate.getMonth() + 1;
+    const fisYearMoStr = '' + fisYear + (fisMonth < 10 ? '0' + fisMonth : fisMonth)
+    const fisYearMoNum = Number(fisYearMoStr);
+    const yearMoMoYear = `${fisYearMoStr};${curMonthName} FY${fisYear}`;
+    yearmos.push({
+      // curYear,
+      // curMonthNum,
+      // curMonthName,
+      // fisYear,
+      // fisMonth,
+      str: fisYearMoStr,
+      num: fisYearMoNum,
+      long: yearMoMoYear
+    });
+  }
 
-  return yearmos.map(str => {
-    return {str: str, num: Number(str)};
-  });
+  return yearmos;
+}
+
+function getMonthNameFromNum(mon) {
+  const months = {
+    '1': 'Jan',
+    '2': 'Feb',
+    '3': 'Mar',
+    '4': 'Apr',
+    '5': 'May',
+    '6': 'Jun',
+    '7': 'Jul',
+    '8': 'Aug',
+    '9': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec',
+  };
+  return months[mon.toString()];
 }
 
 function screenHeightShort(elem) {
