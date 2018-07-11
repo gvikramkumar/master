057bc31dd70e1f33134d381d07754cffa136ab76
dan kahle <dan.kahle@gmail.com>
2018-07-11 10:17:07 -0700

    * submeasure-edit: make tooltip be cui tooltip * business upload: make fulename be text-darkgree as per cui

--- sandbox/pgtest.js
+++ sandbox/pgtest.js
@@ -1,13 +1,15 @@
-
 const pgc = require('../dist/server/lib/database/postgres-conn').pgc;
 
 const a = pgc.promise;
 pgc.promise.then(db => {
-
-  console.log('pg up');
-
-  // this is good, but we need to create a function that takes (schema.table, field, value), and a then function that tests this existence:
-  // function getExists = results => results.rows[0].exists;
+  console.log('postgres is up');
+})
+  .catch(err => {
+    console.log('postgres is down');
+    console.error(err);
+  });
+// this is good, but we need to create a function that takes (schema.table, field, value), and a then function that tests this existence:
+// function getExists = results => results.rows[0].exists;
 
 /*
   db.query('select exists (select 1 from fdscon.vw_fds_products where product_family_id = $1 limit 1)', ['HCS'])
@@ -26,5 +28,4 @@ pgc.promise.then(db => {
     })
 */
 
-})
 
