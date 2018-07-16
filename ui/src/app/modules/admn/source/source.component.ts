dc33c5ca2ea220fb4046f7bc64c47ecfae5f544f
Maryellen Oltman <moltman@cisco.com>
2018-07-16 15:56:14 -0700

    US150510: Source edit form fields fill with existing data

--- ui/src/app/modules/admn/source/source.component.ts
+++ ui/src/app/modules/admn/source/source.component.ts
@@ -3,7 +3,8 @@ import {AppStore} from "../../../app/app-store";
 import {ActivatedRoute} from "@angular/router";
 import {RoutingComponentBase} from "../../../core/base-classes/routing-component-base";
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
-import {LookupService} from '../../_common/services/lookup.service';
+import {SourceService} from '../../_common/services/source.service';
+import {Source} from '../../_common/models/source';
 import {CuiTableOptions} from "@cisco-ngx/cui-components";
 
 @Component({
@@ -13,83 +14,83 @@ import {CuiTableOptions} from "@cisco-ngx/cui-components";
 })
 export class SourceComponent extends RoutingComponentBase implements OnInit {
 
-  //CUI TABLE
-  tableOptions = new CuiTableOptions({
-    bordered: true,
-    columns: [
-      {
-        name: "Name",
-        sortable: true,
-        key: "name"
-      },
-      {
-        name: "Code",
-        sortable: false,
-        key: "code",
-      },
-      {
-        name: "Status",
-        sortable: true,
-        key: "active"
-      }
-    ],
-    striped: true
-
-  });
-
-
-
-
+  sources: Source[] = [];
   sourceName: string;
   sourceDesc: string;
-  sourceStatus: boolean;
-  tableColumns = ['name', 'code', 'active'];
+  sourceStatus: string;
+  sourceStatusBool: boolean;
+  showAdd: boolean = false;
+  showEdit: boolean = false;
+  showForm: boolean = false;
+  tableColumns = ['name', 'desc', 'status'];
   dataSource: MatTableDataSource<Source>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  //sources: Source[] = []; //blank Source array to be filled in ngOnInit
-
-  //SOURCE DUMMY DATA:
-  sources: Source[] = [
-    {
-      id: 1,
-      name: "Source1",
-      code: "src1",
-      active: "A"
-    },
-    {
-      id: 2,
-      name: "Source2",
-      code: "src2",
-      active: "I"
-    }
-  ];
 
   filterValue = '';
 
   constructor(
     private store: AppStore,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sourceService: SourceService
   ) {
     super(store, route);
 
   }
 
   ngOnInit() {
-    this.dataSource = new MatTableDataSource<Source>(this.sources);
+    /*this.dataSource = new MatTableDataSource<Source>(this.sources);
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort;*/
+
+    Promise.all([
+      this.sourceService.getMany().toPromise()
+    ])
+      .then(results => {
+        this.sources = results[0];
+        this.dataSource = new MatTableDataSource<Source>(this.sources);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-}
+  addSource() {
+    this.sourceName = '';
+    this.sourceDesc = '';
+    this.sourceStatus = 'I';
+    this.sourceStatusBool = false;
+    this.showEdit = false;
+    this.showAdd = true;
+    this.showForm = true;
+  }
 
-class Source {
-  id: number;
-  name: string;
-  code: string;
-  active: string;
+  editSource(i) {
+    var source = this.getSourceById(i);
+    this.sourceName = source.name;
+    this.sourceDesc = source.description;
+    this.sourceStatus = source.status;
+    if(this.sourceStatus == 'A') {
+      this.sourceStatusBool = true;
+    }
+    else {
+      this.sourceStatusBool = false;
+    }
+    this.showAdd = false;
+    this.showEdit = true;
+    this.showForm = true;
+  }
+
+  getSourceById(id) {
+    for (var i = 0; i < this.sources.length; i++) {
+      if (this.sources[i]['id'] === id) {
+        return this.sources[i];
+      }
+    }
+    return null;
+  }
+
 }
