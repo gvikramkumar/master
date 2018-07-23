afb82e1a2fb45d7770a8be9cb8b30dec72a5880b
Maryellen Oltman <moltman@cisco.com>
2018-07-23 16:55:23 -0700

    US150510: Hacky solution to cui-select bug on source mapping page (bug is that each cui-select uses the same "sources" array for selected/unselected)

--- ui/src/app/modules/admn/source-mapping/source-mapping.component.ts
+++ ui/src/app/modules/admn/source-mapping/source-mapping.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {SourceService} from '../../_common/services/source.service';
 import {UiUtil} from '../../../core/services/ui-util';
 import {AppStore} from '../../../app/app-store';
 import {RoutingComponentBase} from '../../../core/base-classes/routing-component-base';
 import {DfaModule} from '../../../modules/_common/models/module';
+import {SourceService} from '../../_common/services/source.service';
+import {Source} from '../../_common/models/source';
 import {shUtil} from '../../../../../../shared/shared-util';
+import {MatTableDataSource} from '@angular/material';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'fin-source-mapping',
@@ -15,6 +18,8 @@ import {shUtil} from '../../../../../../shared/shared-util';
 export class SourceMappingComponent extends RoutingComponentBase implements OnInit {
 
   modules: DfaModule[];
+  sources: Source[][] = [];
+  selectedSources: number[][] = [];
 
   constructor(
     private store: AppStore,
@@ -24,14 +29,25 @@ export class SourceMappingComponent extends RoutingComponentBase implements OnIn
     private uiUtil: UiUtil
   ) {
     super(store, route);
-
   }
 
   ngOnInit() {
     this.modules = this.store.modules.filter(module => !shUtil.isAdminModuleId(module.moduleId));
+    this.sourceService.getMany()
+      .subscribe(sources => {
+        // this.sources = sources;
+        this.modules.forEach(() => {
+          this.sources.push(_.cloneDeep(sources));
+        })
+      });
+
   }
 
   sampleItems = ['one','two','three'];
   sample: string;
+
+}
+
+class SourceModuleMap {
 
 }
