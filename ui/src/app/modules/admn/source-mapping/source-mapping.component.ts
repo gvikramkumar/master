57593f56d72872c991e2e3ccdaf0fa0529d715a7
Maryellen Oltman <moltman@cisco.com>
2018-07-23 14:24:04 -0700

    US150510: Add a "select" for each module on sources mapping page

--- ui/src/app/modules/admn/source-mapping/source-mapping.component.ts
+++ ui/src/app/modules/admn/source-mapping/source-mapping.component.ts
@@ -4,6 +4,8 @@ import {SourceService} from '../../_common/services/source.service';
 import {UiUtil} from '../../../core/services/ui-util';
 import {AppStore} from '../../../app/app-store';
 import {RoutingComponentBase} from '../../../core/base-classes/routing-component-base';
+import {DfaModule} from '../../../modules/_common/models/module';
+import {shUtil} from '../../../../../../shared/shared-util';
 
 @Component({
   selector: 'fin-source-mapping',
@@ -11,6 +13,8 @@ import {RoutingComponentBase} from '../../../core/base-classes/routing-component
   styleUrls: ['./source-mapping.component.scss']
 })
 export class SourceMappingComponent extends RoutingComponentBase implements OnInit {
+
+  modules: DfaModule[];
 
   constructor(
     private store: AppStore,
@@ -24,6 +28,10 @@ export class SourceMappingComponent extends RoutingComponentBase implements OnIn
   }
 
   ngOnInit() {
+    this.modules = this.store.modules.filter(module => !shUtil.isAdminModuleId(module.moduleId));
   }
+
+  sampleItems = ['one','two','three'];
+  sample: string;
 
 }
