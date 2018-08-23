933915b1b30fa176a50a43f20e391c856198de77
Maryellen Oltman <moltman@cisco.com>
2018-08-23 15:24:09 -0700

    US211599: implement logical restrictions for input filter level / manual mapping

--- ui/src/app/modules/_common/submeasure/submeasure-edit/submeasure-edit.component.ts
+++ ui/src/app/modules/_common/submeasure/submeasure-edit/submeasure-edit.component.ts
@@ -38,6 +38,16 @@ export class SubmeasureEditComponent extends RoutingComponentBase implements OnI
   COGS = ' Cogs '; // todo: move to lookup
   disableReportingLevels = [];
   disableCategories = false;
+  BeIflDisabled = false;
+  ProductIflDisabled = false;
+  LegalIflDisabled = false;
+  SalesIflDisabled = false;
+  ScmsIflDisabled = false;
+  BeMmDisabled = false;
+  ProductMmDisabled = false;
+  LegalMmDisabled = false;
+  SalesMmDisabled = false;
+  ScmsMmDisabled = false;
   ifl_switch_ibe = false;
   ifl_switch_le = false;
   ifl_switch_p = false;
@@ -309,22 +319,133 @@ export class SubmeasureEditComponent extends RoutingComponentBase implements OnI
   }
 
   iflChange(sw) {
-    // if Internal Business Entity is selected, make Product disabled, and vice-versa
-    if (sw === 'ibe' && this.ifl_switch_ibe && this.ifl_switch_p) {
-      // this.ifl_switch_p = false;
-
-    } else if (sw === 'p' && this.ifl_switch_ibe && this.ifl_switch_p) {
-      // this.ifl_switch_ibe = false;
-
+    // if Internal Business Entity is selected, make Product disabled, and vice-versa.
+    // undo disabled when unselected
+    // if anything is selected in ifl, disable in mm
+    switch (sw) {
+      case 'ibe' :
+        if (this.ifl_switch_ibe) {
+          this.mm_switch_ibe = false;
+          this.ifl_switch_p = false;
+          this.ProductIflDisabled = true;
+          this.BeMmDisabled = true;
+          this.ProductMmDisabled = true;
+          this.sm.inputFilterLevel.productLevel = undefined;
+        } else {
+          this.ProductIflDisabled = false;
+          this.BeMmDisabled = false;
+          this.ProductMmDisabled = false;
+        }
+        break;
+      case 'p' :
+        if (this.ifl_switch_p) {
+          this.ifl_switch_ibe = false;
+          this.BeIflDisabled = true;
+          this.ProductMmDisabled = true;
+          this.sm.inputFilterLevel.internalBELevel = undefined;
+        } else {
+          this.BeIflDisabled = false;
+          this.ProductMmDisabled = false;
+        }
+        break;
+      case 'le' :
+        if (this.ifl_switch_le) {
+          this.LegalMmDisabled = true;
+          this.sm.manualMapping.entityLevel = undefined;
+        } else {
+          this.LegalMmDisabled = false;
+        }
+        break;
+      case 's' :
+        if (this.ifl_switch_s) {
+          this.SalesMmDisabled = true;
+          this.sm.manualMapping.salesLevel = undefined;
+        } else {
+          this.SalesMmDisabled = false;
+        }
+        break;
+      case 'scms' :
+        if (this.ifl_switch_scms) {
+          this.ScmsMmDisabled = true;
+          this.sm.manualMapping.scmsLevel = undefined;
+        } else {
+          this.ScmsMmDisabled = false;
+        }
+        break;
     }
   }
 
   mmChange(sw) {
-    // if Internal Business Entity is selected, make sure Product is unselected
-    if (sw === 'ibe' && this.mm_switch_ibe && this.mm_switch_p) {
-      this.mm_switch_p = false;
-    } else if (sw === 'p' && this.mm_switch_ibe && this.mm_switch_p) {
+    // if Internal Business Entity is selected, disable product
+    // disable corresponding value in ifl
+    
+    switch (sw) {
+      case 'ibe' :
+        if (this.mm_switch_ibe) {
+          this.ifl_switch_ibe = false;
+          this.mm_switch_p = false;
+          this.ProductMmDisabled = true;
+          this.BeIflDisabled = true;
+          this.sm.manualMapping.productLevel = undefined;
+          this.sm.manualMapping.internalBELevel = undefined;
+        } else {
+          this.ProductMmDisabled = false;
+          this.BeIflDisabled = false;
+        }
+        break;
+      case 'p' :
+        if (this.mm_switch_p) {
+          this.mm_switch_ibe = false;
+          this.BeMmDisabled = true;
+          this.ProductIflDisabled = true;
+          this.sm.manualMapping.internalBELevel = undefined;
+          this.sm.inputFilterLevel.productLevel = undefined;
+        } else {
+          this.BeMmDisabled = false;
+          this.ProductIflDisabled = false;
+        }
+        break;
+      case 'le' :
+        if (this.mm_switch_le) {
+          this.LegalIflDisabled = true;
+          this.sm.inputFilterLevel.entityLevel = undefined;
+        } else {
+          this.LegalIflDisabled = false;
+        }
+        break;
+      case 's' :
+        if (this.mm_switch_s) {
+          this.SalesIflDisabled = true;
+          this.sm.inputFilterLevel.salesLevel = undefined;
+        } else {
+          this.SalesIflDisabled = false;
+        }
+        break;
+      case 'scms' :
+        if (this.mm_switch_scms) {
+          this.ScmsIflDisabled = true;
+          this.sm.inputFilterLevel.scmsLevel = undefined;
+        } else {
+          this.ScmsIflDisabled = false;
+        }
+        break;
+    }
+  }
+
+  mmSelect() {
+    // if manual mapping is unselected, clear out the values
+    if (!this.isManualMapping()) {
       this.mm_switch_ibe = false;
+      this.mm_switch_p = false;
+      this.mm_switch_le = false;
+      this.mm_switch_s = false;
+      this.mm_switch_scms = false;
+
+      this.sm.manualMapping.internalBELevel = undefined;
+      this.sm.manualMapping.productLevel = undefined;
+      this.sm.manualMapping.entityLevel = undefined;
+      this.sm.manualMapping.salesLevel = undefined;
+      this.sm.manualMapping.scmsLevel = undefined;
     }
   }
 
