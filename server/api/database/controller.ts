19a2dc6bd9bb3d53ba2e9742f7f9e7cc1ea56abd
dan kahle <dan.kahle@gmail.com>
2018-09-03 15:37:33 -0700

    US215998: all non-upload tables mongoToPgSync working

--- server/api/database/controller.ts
+++ server/api/database/controller.ts
@@ -29,10 +29,10 @@ export default class DatabaseController {
     const log: string[] = [];
     const elog: string[] = [];
     Promise.all([
-      // this.moduleSourceCtrl.mongoToPgSync('dfa_data_sources', req.user.id, log, elog),
-      // this.measureCtrl.mongoToPgSync('dfa_measure', req.user.id, log, elog),
-      // this.moduleCtrl.mongoToPgSync('dfa_module', req.user.id, log, elog),
-      // this.openPeriodCtrl.mongoToPgSync('dfa_open_period', req.user.id, log, elog),
+      this.moduleSourceCtrl.mongoToPgSync('dfa_data_sources', req.user.id, log, elog),
+      this.measureCtrl.mongoToPgSync('dfa_measure', req.user.id, log, elog),
+      this.moduleCtrl.mongoToPgSync('dfa_module', req.user.id, log, elog),
+      this.openPeriodCtrl.mongoToPgSync('dfa_open_period', req.user.id, log, elog),
       // this.deptUploadCtrl.mongoToPgSync('dfa_prof_dept_acct_map_upld', req.user.id, log, elog),
       // this.dollarUploadCtrl.mongoToPgSync('dfa_prof_input_amnt_upld', req.user.id, log, elog),
       // this.mappingUploadCtrl.mongoToPgSync('dfa_prof_manual_map_upld', req.user.id, log, elog),
@@ -49,6 +49,11 @@ export default class DatabaseController {
         const data = {success: log, errors: elog};
         next(Object.assign(err, data));
         return;
+        // this is how we'd not stop for errors (below) along with try/catch in controllerBase.mongoToPgSync()
+        // but if we do this, we don't get stack trace from error. Need that stack trace to find the issue
+        // albeit the try/catch method will show us all the records having issues, not stopping on the first one
+        // so both are useful, the above for debugging code and below for debugging data. You'd need to log and ignore
+        // errors to debug the data, not just jump out on first failure.
         // next(new ApiError('MongoToPgSync Errors', data));
       });
   }
