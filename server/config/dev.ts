5e091701ab03b0f84835e4e2c7edb409ffac41eb
dan kahle <dan.kahle@gmail.com>
2018-09-03 15:24:47 -0700

    US215998: make dev config use sdev postgres

--- server/config/dev.ts
+++ server/config/dev.ts
@@ -3,17 +3,17 @@ export default {
   mongoUri: 'mongodb://localhost/fin-dfa',
   corsOrigin: ['http://localhost:4200'],
   showStack: true,
-/*
   postgres: {
     host: 'finpg-dev-01',
     port: 5432,
     database: 'fpadev',
   }
-*/
+/*
   postgres: {
     host: 'localhost',
     port: 5432,
     database: 'postgres'
   }
+*/
 };
 
