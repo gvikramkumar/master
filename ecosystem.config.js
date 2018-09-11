1e310ffbb913999a7a2b8931123d94c0c3cb1fe1
dan kahle <dan.kahle@gmail.com>
2018-09-11 15:47:35 -0700

    pm2: set min_uptime to avoid restarts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     name      : 'fin-dfa',
     script    : 'dist/server/server.js',
     max_memory_restart: '2G',
-    max_restarts: 1,
+    max_restarts: 16,
+    min_uptime: '10s',
     env: {
       NODE_ENV: 'development'
     },
