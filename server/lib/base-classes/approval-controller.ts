1e99caff4b8fc8f78967ba8ab28afb4761c38dbb
dan kahle <dan.kahle@gmail.com>
2018-09-13 12:29:18 -0700

    US150529: approval workflow rest and controller functions

--- server/lib/base-classes/approval-controller.ts
+++ server/lib/base-classes/approval-controller.ts
@@ -19,6 +19,7 @@ export default class ApprovalController extends ControllerBase {
   }
 
   saveToDraft(req, res, next) {
+    req.body.status = 'D';
     this.addOneNoValidate(req, res, next);
   }
 
