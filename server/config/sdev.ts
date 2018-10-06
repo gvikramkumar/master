364b7a5abc9169154e4ef0822c89552f318d8355
dan kahle <dan.kahle@gmail.com>
2018-10-06 14:43:25 -0700

    bugfix: fix sdev mongo uri

--- server/config/sdev.ts
+++ server/config/sdev.ts
@@ -1,7 +1,7 @@
 export default {
   corsOrigin: [],
   mongo: {
-    uri: 'mongodb://localhost/fin-dfa',
+    uri: 'mongodb://findp-dev-01.cisco.com/fin-dfa',
     host: 'findp-dev-01.cisco.com',
     port: 27017,
     db: 'fin-dfa'
