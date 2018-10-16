26019c6eaa5a6208bef5fa67375ac4e5998ceba1
dan kahle <dan.kahle@gmail.com>
2018-10-16 14:48:50 -0700

    bugfix: fiscalMonth on reports page

--- ui/src/app/modules/admn/database-sync/database-sync.component.ts
+++ ui/src/app/modules/admn/database-sync/database-sync.component.ts
@@ -29,7 +29,7 @@ export class DatabaseSyncComponent extends RoutingComponentBase {
   mongoToPgSync() {
     this.results = null;
     this.uiUtil.genericDialog('Are you sure you want to sync data from Mongo to Postgres?',
-      null, 'Database Sync', DialogType.okCancel)
+      null, 'Database Sync', DialogType.yesNo)
       .subscribe(resp => {
         if (resp) {
           this.databaseService.mongoToPgSync()
@@ -41,7 +41,7 @@ export class DatabaseSyncComponent extends RoutingComponentBase {
   pgToMongoSync() {
     this.results = null;
     this.uiUtil.genericDialog('Are you sure you want to sync data from Postgres to Mongo?',
-      null, 'Database Sync', DialogType.okCancel)
+      null, 'Database Sync', DialogType.yesNo)
       .subscribe(resp => {
         if (resp) {
           this.databaseService.pgToMongoSync()
