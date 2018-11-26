587fb93917eae5a2af5422057c255b9ccc02b764
Kandasamy Pandian <pathimoo@cisco.com>
2018-11-26 15:33:22 -0800

    Working maven project!

---
+++ src/main/java/com/cisco/cool/Application.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.cisco.cool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Application.java - <Describe the intent of this class>
+ * @author K Pandian (pathimoo)
+ * Nov 26, 2018
+ *
+ */
+@Configuration
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        SpringApplication.run(Application.class, args);
+
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(Application.class);
+	}
+	
+}
