ac5a9f058b4609964e61d96f408497f0435528d7
Deepak <dtare@cisco.com>
2018-11-26 21:55:37 -0800

    added org.springframework.boot.autoconfigure

--- src/main/java/com/cisco/cool/Application.java
+++ src/main/java/com/cisco/cool/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 /**
  * Application.java - <Describe the intent of this class>
