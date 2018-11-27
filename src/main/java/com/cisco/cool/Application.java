b9a9c9550ba1933a0c39e9a879dda02821df1c4b
Kandasamy Pandian <pathimoo@cisco.com>
2018-11-26 17:02:22 -0800

    Cleaned pom.xml!

--- src/main/java/com/cisco/cool/Application.java
+++ src/main/java/com/cisco/cool/Application.java
@@ -4,9 +4,7 @@
 package com.cisco.cool;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +17,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class Application extends SpringBootServletInitializer{
 
 	/**
