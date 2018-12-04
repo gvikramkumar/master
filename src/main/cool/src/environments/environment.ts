66e26bdcc5c63322ea98d2b1f66807eb9c92d968
Lulu Feng -X (lulfeng - LARSEN Lulfeng TOUBRO INFOTECH LTD at Cisco) <lulfeng@cisco.com>
2018-12-03 19:32:13 -0800

    updated environment file

--- src/main/cool/src/environments/environment.ts
+++ src/main/cool/src/environments/environment.ts
@@ -8,20 +8,28 @@ export const environment = {
 
   REST_API_URL_GET_CURRENT_USER: 'https://pdaf-api-stg.cisco.com/pdafapp/system/1.0/get/currentUser',
   REST_API_URL:'https://pdaf-api-dev.cisco.com/pdafapp/user/1.0/getLdapUserInfo/',
+
   REST_API_PRIMARY_URL:'https://cool-srv-dev.cisco.com/coolsrv/primaryBusiness/',
   
   REST_API_OFFER_CREATE_URL: 'https://cool-srv-dev.cisco.com/coolsrv/offer/create/',
   
+  REST_API_PRIMARY_BUSINESS_ENTITY_URL: 'https://pdaf-api.cisco.com/pdafapp/mdm/1.0/hierarchy/getBUhierarchy?business_unit=',
   
   REST_API_SECONDARY_BUSINESS_UNIT_URL: 'https://pdaf-api.cisco.com/pdafapp/mdm/1.0/hierarchy/getBUhierarchy?columns=business_unit&distinct=true',
+
+
   REST_API_SECONDARY_BUSINESS_ENTITY_URL: 'https://pdaf-api.cisco.com/pdafapp/mdm/1.0/hierarchy/getBUhierarchy?business_unit=',
  
  
- 
+ REST_API_MMEDIT_SEARCH_URL:'https://cool-srv-dev.cisco.com/coolsrv/collabrators/searchCollabrators',
+
   REST_API_MYACTIONS_URL: 'https://cool-srv-dev.cisco.com/coolsrv/action/getMyAction/',
   REST_API_MYOFFERS_URL: 'https://cool-srv-dev.cisco.com/coolsrv/offer/getMyOffers/',
-  REST_API_MMOFFER_ATTRIBUTES_URL:'http://10.155.72.168:8080/getOfferDefaults?type=offerdimensions',
-  REST_API_MMATTRIBUTES_POST_URL:'http://10.155.72.168:8080/validateOfferDim',
-  REST_API_MM_STAKEHOLDERS_GET_URL:'https://cool-srv-dev.cisco.com/coolsrv/stakeholder/getStakeHolderMgnt/'
+  REST_API_MMOFFER_ATTRIBUTES_URL:'https://cool-srv-dev.cisco.com/coolsrv/getOfferDefaults?type=offerdimensions',
+  REST_API_MMATTRIBUTES_POST_URL:'https://cool-srv-dev.cisco.com/coolsrv/validateOfferDim',
+  REST_API_MM_STAKEHOLDERS_GET_URL:'https://cool-srv-dev.cisco.com/coolsrv/stakeholder/getStakeHolderMgnt/',
+  REST_API_MM_OFFER_BUILDER_GET_URL:'https://cool-srv-dev.cisco.com/coolsrv/offer/getOffersDetails/',
+  REST_API_MM_STAKEHOLDERS_SEARCH_URL: 'https://cool-srv-dev.cisco.com/coolsrv/collabrators/searchCollabrators',
+  REST_API_MM_STAKEHOLDERS_EDIT_ADD_URL:'https://cool-srv-dev.cisco.com/coolsrv/offer/getMyOffers/ekuruva'
   // change with localhost:8080 in local dev
 };
