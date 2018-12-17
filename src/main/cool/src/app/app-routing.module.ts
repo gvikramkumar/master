2fd2b3fc5f330ea49019f324bd58cdc2846940b1
Eeranna Kuruva <ekuruva@cisco.com>
2018-12-17 10:06:22 +0530

    Admin Screens for Access Management Functionality

--- src/main/cool/src/app/app-routing.module.ts
+++ src/main/cool/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { OfferDetailViewComponent } from './offer-detail-view/offer-detail-view.
 import { StrategyReviewComponent } from './strategy-review/strategy-review.component';
 import {ExitCriteriaValidationComponent} from './exit-criteria-validation/exit-criteria-validation.component';
 import { CreateNewActionComponent } from './create-new-action/create-new-action.component';
+import { AccessManagementComponent } from './access-management/access-management.component';
 
 const routes: Routes = [
   {
@@ -51,6 +52,10 @@ const routes: Routes = [
   {
     path: 'strategyReview/:id',
     component: StrategyReviewComponent
+  },
+  {
+    path: 'accessManagement',
+    component: AccessManagementComponent
   }
 ];
 
