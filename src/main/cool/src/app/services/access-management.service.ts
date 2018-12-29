081b4ca3fb7d2d617d55b03a596bbc66674d35d7
Eeranna Kuruva <ekuruva@cisco.com>
2018-12-29 16:28:14 +0530

    Populating Business Units & Business entities, Passing Array of user to Post Request

--- src/main/cool/src/app/services/access-management.service.ts
+++ src/main/cool/src/app/services/access-management.service.ts
@@ -4,9 +4,12 @@ import { environment } from '../../environments/environment';
 import { AccessManagement } from '../models/accessmanagement';
 import { Observable } from 'rxjs/Observable';
 import { EnvironmentService } from '../../environments/environment.service';
+import { NewUser } from '../models/newuser';
 
 @Injectable()
 export class AccessManagementService {
+    businessUnitUrl: string = environment.REST_API_SECONDARY_BUSINESS_UNIT_URL;
+    businessEntityUrl: string = environment.REST_API_SECONDARY_BUSINESS_ENTITY_URL;
 
     constructor(private httpClient: HttpClient, private environmentService: EnvironmentService) {}
 
@@ -14,13 +17,31 @@ export class AccessManagementService {
         return this.httpClient.get(this.environmentService.REST_API_ACCESS_MANAGEMENT_GETALL_URL);
     }
 
-    registerUser(accessManagement: AccessManagement): Observable<any> {
+    registerUser(newUser: NewUser[]): Observable<any> {
         return this.httpClient.post(this.environmentService.REST_API_ACCESS_MANAGEMENT_CREATEUSER_URL,
-                                    accessManagement, { withCredentials: true });
+            newUser, { withCredentials: true });
     }
 
     updateAccessManagement(accessManagement: AccessManagement) {
         return this.httpClient.post(this.environmentService.REST_API_ACCESS_MANAGEMENT_UPDATEUSER_URL,
                                     accessManagement, { withCredentials: true });
+    }
+
+    /**
+     * Function to get distinct business units from pdaf service
+     */
+    getBusinessUnit() {
+        const url = this.businessUnitUrl;
+        return this.httpClient.get(url, {withCredentials:true});
+    }
+
+    /**
+     * Function to get business entities for the selected business units, from
+     * pdaf service
+     * @param bus
+     */
+    getBusinessEntity(bus: string): Observable<any> {
+        const url = this.businessEntityUrl + bus;
+        return this.httpClient.get(url, {withCredentials:true});
     }
 }
