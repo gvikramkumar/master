82a6904889580765ffdea7fc9e130743ebcc8f3b
dan kahle <dan.kahle@gmail.com>
2019-01-04 15:39:26 -0800

    ram: measureId change: v&0 >> measureId 3, supply chain: measureId: 5

--- database/updates/update_12.js
+++
@@ -1,9 +0,0 @@
-
-print('host: ' + host + ', port: ' + port + ', db: ' + _db);
-
-const conn = new Mongo(host + ':' + port);
-const db = conn.getDB(_db);
-
-db.dfa_measure.updateOne({name: 'Manufacturing V&O'}, {$unset: {measureId: ''}})
-db.dfa_measure.updateOne({name: 'Warranty'}, {$set: {measureId: NumberInt(6)}})
-db.dfa_measure.updateOne({name: 'Manufacturing V&O'}, {$set: {measureId: NumberInt(5)}})
