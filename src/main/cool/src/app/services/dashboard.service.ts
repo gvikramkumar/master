0fefa2aa34b18ba089d6703f7a0bceabb35832dd
Prema Veeramanie <preveera@cisco.com>
2019-01-08 13:29:24 -0800

    fixed dismiss Notification

--- src/main/cool/src/app/services/dashboard.service.ts
+++ src/main/cool/src/app/services/dashboard.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
-import {UserService} from './user.service';
+import { UserService } from './user.service';
 import { EnvironmentService } from '../../environments/environment.service';
 
 @Injectable()
 
 
 export class DashboardService {
-  
- constructor(private http:HttpClient, private userService: UserService, private environmentService: EnvironmentService) { }
+
+  constructor(private http: HttpClient, private userService: UserService, private environmentService: EnvironmentService) { }
 
   getMyActionsList(): Observable<any> {
     let url = this.environmentService.REST_API_ACTIONSTRACKER_URL+this.userService.getUserId()+'/false';
@@ -18,22 +18,11 @@ export class DashboardService {
   }
 
   getMyOffersList(): Observable<any> {
-    let url = this.environmentService.REST_API_MYOFFERS_URL+this.userService.getUserId();
-    return this.http.get(url,{ withCredentials: true });
+    let url = this.environmentService.REST_API_MYOFFERS_URL + this.userService.getUserId();
+    return this.http.get(url, { withCredentials: true });
   }
 
-  postDismissNotification(data): Observable<any> {
-    
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Accept' : 'application/json'
-      }),
-      withCredentials: true
-    };
-    let url = this.environmentService.REST_API_DISMISS_NOTIFICATION;
-    console.log("url:::",url);
-    return this.http.post(url, data,httpOptions);
-}
+  postDismissNotification(data) {
+    return this.http.post(this.environmentService.REST_API_DISMISS_NOTIFICATION, data);
+  }
 }
