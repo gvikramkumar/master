a72d8f983977a50856dd224e307d22687b5266ef
Eeranna Kuruva <ekuruva@cisco.com>
2019-01-09 22:47:43 +0530

    Stake holder identification alignment in offer overview details page

---
+++ src/main/cool/src/app/directives/stakeholder-identification/stakeholder-identification.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-stakeholder-identification',
+  templateUrl: './stakeholder-identification.component.html',
+  styleUrls: ['./stakeholder-identification.component.css']
+})
+export class StakeholderIdentificationComponent implements OnInit {
+  @Input() subgroupName: string;
+  @Input() characteristics: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
