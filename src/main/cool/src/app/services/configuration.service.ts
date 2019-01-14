b8b6203d9caeedd6425b2f0bddd2b9391afd570f
Lulu Feng -X (lulfeng - LARSEN Lulfeng TOUBRO INFOTECH LTD at Cisco) <lulfeng@cisco.com>
2019-01-14 13:58:38 -0800

    merge with latest code for pdaf change

--- src/main/cool/src/app/services/configuration.service.ts
+++ src/main/cool/src/app/services/configuration.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { UserService } from './user.service';
 
@@ -12,40 +12,45 @@ export class ConfigurationService {
     urlGetUserInfo = this.environmentService.REST_API_URL_GET_LDAP_INFO;
     urlGetCurrentUser = this.environmentService.REST_API_URL_GET_CURRENT_USER;
     urlCheckAdminAccess = this.environmentService.REST_API_ACCESS_MANAGEMENT_ACCESS_CHECK_URL;
-    accessToken:string = "";
-    private _startupData: any;
+    private _startupData: any = {};
 
-    constructor(private httpClient: HttpClient, 
-        private userService: UserService, 
+    constructor(private httpClient: HttpClient,
+        private userService: UserService,
         private environmentService: EnvironmentService,
         private accessMgmtService: AccessManagementService) {
     }
 
     init() {
-        
         return new Promise((resolve, reject) => {
-            this.httpClient.get(this.urlGetCurrentUser, { withCredentials: true }).toPromise()
-                .then((user) => {
-                    this.userService.setUserId(user);
-                    
-                    // check for admin access
-                    this.accessMgmtService.checkAdminAccess().toPromise().then((data) => {
-                        console.log(data);
-                        this._startupData = {hasAdminAccess:true, userName: data.userName};
-                    }, (err) => {
-                        console.log(err);
-                        this._startupData = {hasAdminAccess:false};
-                    });
-                    
-                    return this.httpClient.post(this.urlGetUserInfo, { userId: user }, { withCredentials: true }).toPromise().then((res: any) => {
-                        console.log(res);
-                        this.userService.setFirstName(res.firstName);
-                        this.userService.setLastName(res.lastName);
-                    })
-                })
-                .then((response) => resolve(true))
-                .catch(this.handleError());
+            if (this._startupData.token || window.location.hash.indexOf('access_token') !== -1) {
+                this._startupData.token = window.location.hash.split("&")[0].split("=")[1];
 
+                this.httpClient.get(this.urlGetCurrentUser).toPromise()
+                    .then((user) => {
+                        this.userService.setUserId(user);
+
+                        // check for admin access
+                        this.accessMgmtService.checkAdminAccess().toPromise().then((data) => {
+                            console.log(data);
+                            this._startupData.hasAdminAccess = true;
+                            this._startupData.userName = data.userName;
+                        }, (err) => {
+                            console.log(err);
+                            this._startupData.hasAdminAccess = false;
+                        });
+
+                        return this.httpClient.post(this.urlGetUserInfo, { userId: user }).toPromise().then((res: any) => {
+                            console.log(res);
+                            this.userService.setFirstName(res.firstName);
+                            this.userService.setLastName(res.lastName);
+                        })
+                    })
+                    .then((response) => resolve(true))
+                    .catch(this.handleError());
+            } else {
+                let url = this.environmentService.GENERATE_AUTH_TOKEN_URL;
+                window.location.replace(url);
+            }
         })
     }
 
@@ -54,8 +59,6 @@ export class ConfigurationService {
             console.log(error);
         }
     }
-
-   
 
     get startupData(): any {
         return this._startupData;
