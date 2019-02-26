b05337ed3959880e3643eaf95e7c0e4c46e25bef
shrsudha <shrsudha@cisco.com>
2019-02-25 20:47:33 -0800

    - Add functionalRole Field In POJO of setOfferSolution

--- src/main/cool/src/app/mm-assesment/mm-assesment.component.ts
+++ src/main/cool/src/app/mm-assesment/mm-assesment.component.ts
@@ -912,9 +912,7 @@ export class MmAssesmentComponent implements OnInit {
 
   proceedToOfferSolution(withRouter = true) {
 
-    let postOfferSolutioningData = {};
-    postOfferSolutioningData['offerId'] = this.currentOfferId == null ? '' : this.currentOfferId;
-
+   
     let groups = [];
     let groupDataWithFirst = [];
     groupDataWithFirst.push(this.dimensionFirstGroupData);
@@ -947,8 +945,11 @@ export class MmAssesmentComponent implements OnInit {
       groups.push(curGroup);
     });
 
+    let postOfferSolutioningData = {};
     postOfferSolutioningData['groups'] = groups;
+    postOfferSolutioningData['functionalRole'] = this.configService.startupData.functionalRole;
     postOfferSolutioningData['mmModel'] = this.currentMMModel == null ? '' : this.currentMMModel;
+    postOfferSolutioningData['offerId'] = this.currentOfferId == null ? '' : this.currentOfferId;
     postOfferSolutioningData['mmMapperStatus'] = this.message['contentHead'];
     console.log('postForOfferSolutioning Data:', postOfferSolutioningData);
 
@@ -958,7 +959,7 @@ export class MmAssesmentComponent implements OnInit {
       postRuleResultData['offerId'] = this.currentOfferId;
       this.monetizationModelService.postRuleResult(postRuleResultData).subscribe(res => { });
 
-      let proceedToStakeholderPostData = {};
+      let proceedToStakeholderPostData = {};     
       proceedToStakeholderPostData['offerId'] = this.currentOfferId == null ? '' : this.currentOfferId;
       proceedToStakeholderPostData['offerName'] = this.offerBuilderdata['offerName'] == null ? '' : this.offerBuilderdata['offerName'];
       proceedToStakeholderPostData['offerDesc'] = this.offerBuilderdata['offerDesc'] == null ? '' : this.offerBuilderdata['offerDesc'];
