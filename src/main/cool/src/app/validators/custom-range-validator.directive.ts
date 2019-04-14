52f894514e6c837c4b0b8d3f2d43408c30fb39fd
shrsudha <shrsudha@cisco.com>
2019-04-13 17:33:59 -0700

    - Restructure

--- src/main/cool/src/app/validators/custom-range-validator.directive.ts
+++ src/main/cool/src/app/validators/custom-range-validator.directive.ts
@@ -1,10 +1,12 @@
 import { Directive } from '@angular/core';
-import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
+import { Validator, ValidatorFn, AbstractControl, NG_VALIDATORS } from '@angular/forms';
+
 import * as _ from 'lodash';
 
 @Directive({
   selector: '[appCustomRangeValidator]',
   providers: [{ provide: NG_VALIDATORS, useExisting: CustomRangeValidatorDirective, multi: true }]
+  // 
 })
 export class CustomRangeValidatorDirective implements Validator {
 
@@ -14,7 +16,8 @@ export class CustomRangeValidatorDirective implements Validator {
 
     const elementValue = control.value;
 
-    const reg = new RegExp('^[0-9]{5}$');
+    const reg = new RegExp('^[0-9]{1,5}(\.[0-9]{2})?$');
+
     if (!reg.test(elementValue)) {
       return { 'cus_pattern': 'Value should be 5 digit number' };
     }
