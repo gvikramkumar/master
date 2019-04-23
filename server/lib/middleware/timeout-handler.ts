fe74050b68a80af399954bbc1544af93da3fee06
khizer <khizerrocks.demon@gmail.com>
2019-04-24 00:24:26 +0530

    DE240931 changed req.path to req.originalUrl

--- server/lib/middleware/timeout-handler.ts
+++ server/lib/middleware/timeout-handler.ts
@@ -5,10 +5,10 @@ export function timeoutHandler() {
   return function (req, res, next) {
     setTimeout(() => {
       if (!res.headersSent) {
-        if (/^\/api\/[a-z]{4}\/upload$/.test(req.baseUrl)) {
-          next(new ApiError('Your request has timed out. Please check email for success/fail status', null));
+        if (/^\/api\/[a-z]{4}\/upload/.test(req.originalUrl)) {
+          next(new ApiError('Your upload request has timed out, but is still being processed. Please check email for success/fail status'));
         } else {
-          next(new ApiError('Your request has timed out', null));
+          next(new ApiError('Your request has timed out'));
         }
       }
     }, config.expressTimeout);
