c1f59a7ace9b53d152d7e10876f7c08b84940bbc
Tamal Choudhury <tamachou@cisco.com>
2019-05-02 16:09:21 -0700

    dynamic-form-question.component.ts edited online with Bitbucket

--- src/main/cool/src/app/construct/dynamic-form-question/dynamic-form-question.component.ts
+++ src/main/cool/src/app/construct/dynamic-form-question/dynamic-form-question.component.ts
@@ -3,38 +3,35 @@ import { FormGroup } from '@angular/forms';
 import { OfferConstructService } from '@app/services/offer-construct.service';
 import { BsDatepickerConfig,BsDaterangepickerDirective } from 'ngx-bootstrap/datepicker';
 import * as moment from 'moment';
-
-
 @Component({
-  selector: 'app-dynamic-form-question',
-  templateUrl: './dynamic-form-question.component.html',
-  styleUrls: ['./dynamic-form-question.component.css']
+selector: 'app-dynamic-form-question',
+templateUrl: './dynamic-form-question.component.html',
+styleUrls: ['./dynamic-form-question.component.css']
 })
 export class DynamicFormQuestionComponent implements OnInit {
-  @Input() question: any;
-  @Input() questionForm: FormGroup;
-  @Input() offerForm: FormGroup;
-  @Input() headerName: FormGroup;
-  @ViewChild('dp4') datepicker: BsDaterangepickerDirective;
-
-  minDate: Date;
-  constructor(public offerConstructService: OfferConstructService) {
-  }
-  ngOnInit() { 
-    this.minDate = new Date();
-  }
-  dateFormat(val){
-    if(val!==''){
-      try{
-        return moment(val).format('DD-MMM-YYYY');
-      }catch(err){}
-    }
-  }
-  updateDate(e){
-    if(e!==''){
-      try{
-        return moment(e).toISOString();
-      }catch(err){}
-    }
-  }
+@Input() question: any;
+@Input() questionForm: FormGroup;
+@Input() offerForm: FormGroup;
+@Input() headerName: FormGroup;
+@ViewChild('dp4') datepicker: BsDaterangepickerDirective;
+minDate: Date;
+constructor(public offerConstructService: OfferConstructService) {
+}
+ngOnInit() { 
+this.minDate = new Date();
+}
+dateFormat(val){
+if(val!==''){
+try{
+return moment(val).format('DD-MMM-YYYY');
+}catch(err){}
+}
+}
+updateDate(e){
+if(e!==''){
+try{
+return moment(e).toISOString();
+}catch(err){}
+}
+}
 }
