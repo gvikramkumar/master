8f7b194aca26fa95443b2128de8af155dae70b3e
prandasa <prandasa@cisco.com>
2019-05-03 16:26:24 -0700

    outline for pricing service module

---
+++ src/main/cool/src/app/pirate-ship/modules/service-annuity-pricing/service-annuity-pricing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiceAnnuityPricingComponent } from './service-annuity-pricing.component';
+
+@NgModule({
+  declarations: [ServiceAnnuityPricingComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServiceAnnuityPricingModule { }
