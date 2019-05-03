fde546bc73942a9da0d01c6699337b8e92931b94
prandasa <prandasa@cisco.com>
2019-05-03 16:39:44 -0700

    update routing parameters

--- src/main/cool/src/app/pirate-ship/modules/service-annuity-pricing/service-annuity-pricing.module.ts
+++ src/main/cool/src/app/pirate-ship/modules/service-annuity-pricing/service-annuity-pricing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServiceAnnuityPricingComponent } from './service-annuity-pricing.component';
+import { MenuBarModule } from '@app/menu/menu-bar.module';
+import { SharedModule } from 'primeng/primeng';
+import { TaskBarModule } from '@app/taskbar/task-bar.module';
+import { RightPanelModule } from '@app/right-panel/right-panel.module';
+import { OfferDetailModule } from '@app/offer-detail/offer-detail.module';
+import { RouterModule } from '@angular/router';
+import { SERVICE_ANNUITY_PRICING_ROUTES } from './service-annuity-pricing.routes';
 
 @NgModule({
   declarations: [ServiceAnnuityPricingComponent],
   imports: [
-    CommonModule
+    SharedModule,
+    TaskBarModule,
+    MenuBarModule,
+    RightPanelModule,
+    OfferDetailModule,
+    RouterModule.forChild(SERVICE_ANNUITY_PRICING_ROUTES)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class ServiceAnnuityPricingModule { }
