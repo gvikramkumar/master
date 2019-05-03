334e934abed0adc0954dd2e4a12a2bdd951b3199
venyerra <venyerra@cisco.com>
2019-05-02 18:04:07 -0700

    validations

--- src/main/cool/src/app/construct/dynamic-form-question/dynamic-form-question.component.ts
+++ src/main/cool/src/app/construct/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,27 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { OfferConstructService } from '@app/services/offer-construct.service';
-import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
-
+import { BsDatepickerConfig,BsDaterangepickerDirective } from 'ngx-bootstrap/datepicker';
+import * as moment from 'moment';
 @Component({
-  selector: 'app-dynamic-form-question',
-  templateUrl: './dynamic-form-question.component.html',
-  styleUrls: ['./dynamic-form-question.component.css']
+selector: 'app-dynamic-form-question',
+templateUrl: './dynamic-form-question.component.html',
+styleUrls: ['./dynamic-form-question.component.css']
 })
 export class DynamicFormQuestionComponent implements OnInit {
   @Input() question: any;
   @Input() questionForm: FormGroup;
   @Input() offerForm: FormGroup;
   @Input() headerName: FormGroup;
-  minDate: Date;
+  @ViewChild('dp4') datepicker: BsDaterangepickerDirective;
 
+  minDate: Date;
   constructor(public offerConstructService: OfferConstructService) {
   }
-
   ngOnInit() { 
     this.minDate = new Date();
   }
-
-
-
+  dateFormat(val){
+    if(val!==''){
+      try{
+        return moment(val).format('DD-MMM-YYYY');
+      }catch(err){}
+   }
+  }
+  updateDate(e){
+    if(e!==''){
+      try{
+        return moment(e).toISOString();
+      }catch(err){}
+    }
+  }
+//Tamal
 }
