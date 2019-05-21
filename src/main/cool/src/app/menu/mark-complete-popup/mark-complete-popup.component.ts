0961ac1a8f314606da7c464d20ac1171a2a71714
lulfeng <lulfeng@cisco.com>
2019-05-20 20:15:09 -0700

    pop up for mark complete

---
+++ src/main/cool/src/app/menu/mark-complete-popup/mark-complete-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mark-complete-popup',
+  templateUrl: './mark-complete-popup.component.html',
+  styleUrls: ['./mark-complete-popup.component.css']
+})
+export class MarkCompletePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
