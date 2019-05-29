a93d5723a41f96537b16a99fc416b65ee7f499cb
shrsudha <shrsudha@cisco.com>
2019-05-29 15:47:25 -0700

    - SSO - Error Handling

--- src/main/cool/src/app/pirate-ship/modules/self-service-orderability/self-service-orderability.component.ts
+++ src/main/cool/src/app/pirate-ship/modules/self-service-orderability/self-service-orderability.component.ts
@@ -107,11 +107,11 @@ export class SelfServiceOrderabilityComponent implements OnInit, OnDestroy {
             this.atoNames.push(dropDownValue.itemName);
           });
 
-        this.postPirateShipDashBoardNotification();
-
-        this.sso = this.ssoList.find(ato => ato.itemName === this.selectedAto);
-        const currentAtoStatus = _.isEmpty(this.sso.orderabilityCheckStatus) ? '' : this.sso.orderabilityCheckStatus;
+        this.sso = _.find(this.ssoList, ['orderabilityCheckStatus', this.selectedAto]);
+        const currentAtoStatus = _.isEmpty(this.sso) ? '' : this.sso.orderabilityCheckStatus;
         this.disableOrderabilityButton = this.disableOrderabilityButton ? this.orderabilityButtonStatus(currentAtoStatus) : false;
+
+        this.postPirateShipDashBoardNotification();
 
       });
 
@@ -144,7 +144,6 @@ export class SelfServiceOrderabilityComponent implements OnInit, OnDestroy {
     window.open(urlToOpen, '_blank');
 
   }
-
 
   // -------------------------------------------------------------------------------------------------------------------
 
