be4b7614773c9bfd27fe4d88e6641a5b1c1f64a2
rchilka <raviraj28900@gmail.com>
2019-06-19 15:28:26 -0700

    US318971 - Email notification on select roles

--- src/main/cool/src/environments/environment.service.ts
+++ src/main/cool/src/environments/environment.service.ts
@@ -16,8 +16,12 @@ export class EnvironmentService {
     idpClientId: string;
     idpClientSecret: string;
 
+    // EMAIL NOTIFICATION URL
+    REST_API_POST_USER_DETAILS_FOR_EMAIL_URL:string;
+
     // CEPM URL
     REST_API_GET_CEPMROLES_URL:string;
+
     //  AUTHENTICATION URLs
     REST_API_AUTH_IDP_TOKEN_URL: string;
     REST_API_GENERATE_AUTH_TOKEN_URL: string;
@@ -296,8 +300,12 @@ export class EnvironmentService {
                 this.tncOwbUrl = 'https://offer-stage.cloudapps.cisco.com/owb/#/subscription';
         }
 
-        //-------------------------CEPM URL------------------------------//
+        //-----------------------EMAIL NOTIFICATION URL --------------------
+        this.REST_API_POST_USER_DETAILS_FOR_EMAIL_URL = `${this.baseApiUrl}/accessManagement/sendAccessManagementEmail`
+
+        //-------------------------CEPM URL------------------------------
         this.REST_API_GET_CEPMROLES_URL = `${this.baseApiUrl}/access/fetchCEPMRoles`
+        
         // ----------------------- AUTHENTICATION URLs -----------------------------------------------
 
         // tslint:disable-next-line: max-line-length
