b8692471e12b16bbe1375174989ff2b23a7c5586
venkata sudeepthi yerramsetty <venyerra@cisco.com>
2019-06-25 16:22:36 -0700

    error message for offer construct for existing pid and spelling and grammer correction

--- src/main/cool/src/app/feature/construct/offer-construct-canvas/offer-construct-canvas.component.ts
+++ src/main/cool/src/app/feature/construct/offer-construct-canvas/offer-construct-canvas.component.ts
@@ -770,7 +770,7 @@ export class OfferconstructCanvasComponent implements OnInit, OnDestroy {
   handleChange(obj, $event) {
     const newValue = $event.target.value;
     this.offerConstructCanvasService.validatePID(newValue).subscribe((data) => {
-      if(data.length == 0){
+      if(data.length > 0){
           obj.validationMessage = "Item Name already exists, please type another name";
           obj.isvalid = false;
           obj.name = newValue;
