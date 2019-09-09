fa8c86b2ac391dee56f9b22b4a7e14c8d673c2e1
Karthikeyan Mohan -X (kmohan3 - LARSEN Kmohan3 TOUBRO INFOTECH LTD at Cisco) <kmohan3@cisco.com>
2019-09-09 11:55:42 +0530

    module based sync push is done

--- server/api/pg-lookup/repo.ts
+++ server/api/pg-lookup/repo.ts
@@ -1083,17 +1083,18 @@ export default class PgLookupRepo {
 
   getETLAndAllocationFlags() {
     const sql = `
-      select dl_processed_flag, alloc_processed_flag 
+      select module_id, dl_processed_flag, alloc_processed_flag 
       from fpadfa.dfa_data_ctrl
-      where module_id = 1
+      --where module_id = 1
       `;
     return pgc.pgdb.query(sql)
       .then(results => {
-        const flags = results.rows[0] || {};
-        return {
-          etlRunning: flags.dl_processed_flag !== 'Y',
-          allocationRunning: flags.alloc_processed_flag !== 'Y'
-        };
+        const flags = results.rows || {};
+        return flags;
+        // return {
+        //   etlRunning: flags.dl_processed_flag !== 'Y',
+        //   allocationRunning: flags.alloc_processed_flag !== 'Y'
+        // };
       });
   }
 
