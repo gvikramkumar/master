e9d6cdba38ef72196d629a5df481fafaf6b54874
Sagar Gangula <saggangu@cisco.com>
2019-09-12 11:12:05 +0530

    Permissions changed

--- database/seed.sh
+++ database/seed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## must be called with: host port database
+### must be called with: host port database
 
 mongo --nodb  --eval "var host='$1', port='$2', _db='$3', user='$4', pass='$5'" create-collections.js
 
