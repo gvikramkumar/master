ef13da320820589cc345c3d19d0340615ebc7fda
Sagar Gangula <saggangu@cisco.com>
2019-09-19 15:18:08 +0530

    Reports and Business Uploads

--- ui/src/app/modules/_common/business-upload/business-upload/business-upload.component.ts
+++ ui/src/app/modules/_common/business-upload/business-upload/business-upload.component.ts
@@ -47,7 +47,7 @@ export class BusinessUploadComponent extends RoutingComponentBase implements OnI
     {type: 'service-map-upload', text: 'Service Mapping Split Percentage', disabled: false, moduleId:1},
     {type: 'service-training-upload', text: 'Service Training Mapping Split Percentage', disabled: false, moduleId:1},
     {type:'misc-exception-upload',text:'Misc Exception Mapping', disabled: false, moduleId:1},
-    {type:'disti-sl3-to-direct-sl2-mapping-upload',text:'Disti SL3 to Direct SL2 Mapping', disabled: false, moduleId:4}
+    {type:'distisl3-to-directsl2-mapping-upload',text:'Disti SL3 to Direct SL2 Mapping', disabled: false, moduleId:4}
   ];
   uploadType: UploadType;
 
