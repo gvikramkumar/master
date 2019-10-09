29137d67c4f4203bf259bdcaa1c620fe67b44db8
Sagar Gangula <saggangu@cisco.com>
2019-10-09 19:01:13 +0530

    Added fiscal month

--- server/api/tsct/upload/distisl3-to-directsl2/import.ts
+++ server/api/tsct/upload/distisl3-to-directsl2/import.ts
@@ -4,13 +4,13 @@ export default class Distisl3ToDirectsl2UploadImport {
   driverSl2: string;
   sourceSl2: string;
   externalTheater: number;
-  //fiscalMonth: number;
+  fiscalMonth: number;
 
   constructor(row, fiscalMonth) {
     this.driverSl2 = row[0] && String(row[0]);
     this.sourceSl2 = row[1] && String(row[1]);
     this.externalTheater = row[2] && Number(row[2]);
-    //this.fiscalMonth = fiscalMonth;
+    this.fiscalMonth = fiscalMonth;
 
     svrUtil.trimStringProperties(this);
   }
